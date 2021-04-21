7d8e3bf76b8637af5d76db40cdd7235565c9a62b
Joseph <serinojoseph@gmail.com>
2021-04-20 23:02:24 -0700

    added onclicks to html

--- home.js
+++ home.js
@@ -1,51 +1,12 @@
 let square = document.getElementById("square");
 
-let birthdayButton = document.getElementById("birthdayButton");
-let congratsButton = document.getElementById("congratsButton");
-let getWellButton = document.getElementById("getWellButton");
-
-let mistyRoseButton = document.getElementById("mistyRoseButton");
-let greenButton = document.getElementById("greenButton");
-let blueButton = document.getElementById("blueButton");
-
-let redTextButton = document.getElementById("redTextButton");
-let greenTextButton = document.getElementById("greenTextButton");
-let blueTextButton = document.getElementById("blueTextButton");
-
-let pixel40Button = document.getElementById("pixel40Button");
-let pixel60Button = document.getElementById("pixel60Button");
-let pixel80Button = document.getElementById("pixel80Button");
-
 let textBox = document.getElementById("textBox");
-
-let paragraph = document.getElementById("paragraph");
-
-let backgroundColorPicker = document.getElementById("backgroundColorPicker");
 
 let fontSizeSlider = document.getElementById("fontSizeSlider");
 
-birthdayButton.addEventListener("click", sayHappyBirthday);
-congratsButton.addEventListener("click", sayCongrats);
-getWellButton.addEventListener("click", sayGetWell);
-
-mistyRoseButton.addEventListener("click", changeBackgroundMisty);
-greenButton.addEventListener("click", changeBackgroundSpring);
-blueButton.addEventListener("click", changeBackgroundSky);
-
-redTextButton.addEventListener("click", changeTextRed);
-greenTextButton.addEventListener("click", changeTextGreen);
-blueTextButton.addEventListener("click", changeTextBlue);
-
-pixel40Button.addEventListener("click", changePixelForty);
-pixel60Button.addEventListener("click", changePixelSixty);
-pixel80Button.addEventListener("click", changePixelEighty);
+let paragraph = document.getElementById("paragraph");
 
 textBox.addEventListener("input", showText);
-
-backgroundColorPicker.addEventListener("input", changeBackgroundColor);
-
-fontSizeSlider.addEventListener("input", changeFontSize);
-
 
 function sayHappyBirthday() {
   square.innerText = "Happy Birthday!";
@@ -84,14 +45,17 @@ function changeTextBlue() {
 }
 
 function changePixelForty() {
+  fontSizeSlider.value = 40;
   square.style.fontSize = "40px";
 }
 
 function changePixelSixty() {
+  fontSizeSlider.value = 60;
   square.style.fontSize = "60px";
 }
 
 function changePixelEighty() {
+  fontSizeSlider.value = 80;
   square.style.fontSize = "80px";
 }
 
@@ -99,11 +63,12 @@ function showText() {
   square.innerHTML = textBox.value;
 }
 
-function changeBackgroundColor() {
+function customBackgroundColor() {
   square.style.backgroundColor = backgroundColorPicker.value;
 }
 
-function changeFontSize() {
+function customFontSize() {
   square.style.fontSize = fontSizeSlider.value + "px";
+
 }
 
